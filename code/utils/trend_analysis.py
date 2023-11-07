d6021767ad2940843efc4d37cb9049ec55fe805a
Lena Mueller <lenamariemue@gmail.com>
2023-11-07 14:07:03 +0100

    ✅ issue2

--- code/utils/trend_analysis.py
+++ code/utils/trend_analysis.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import scipy
@@ -6,41 +7,26 @@ from collections import namedtuple
 from utils.binned_stats import mean
 from utils.primary_stats import hyd_years
 
+def __preprocess(df: pd.DataFrame, which: str):
+    if which == "monthly":
+        x = df["Durchfluss_m3s"].tolist()
+        t = np.arange(hyd_years(df)[0], hyd_years(df)[-1]+1, 1/12)
+        n = len(t)
+    elif which == "yearly":
+        x = mean(df, which="yearly")
+        t = np.arange(hyd_years(df)[0], hyd_years(df)[-1]+1, 1)
+        n = len(t)
+    else:
+        raise ValueError("which must be 'monthly' or 'yearly'")
+    
+    return x, t, n
 
 def linreg(df: pd.DataFrame, which: str):
-    """Returns slope, intercept, r, p, std_err of the linear regression model."""
-    if which == "monthly":
-        x = mean(df, which="monthly")
-        t = np.arange(1, len(x)+1, 1)
-    elif which == "yearly":
-        x = mean(df, which="yearly")
-        t = hyd_years(df)
-    else:
-        raise ValueError("which must be 'monthly' or 'yearly'")
-    return scipy.stats.linregress(t, x, alternative="two-sided")
-
-def coeff_of_determination(df: pd.DataFrame, which: str):
-    """Returns the coefficient of determination."""
-    if which == "monthly":
-        _, _, r, _, _ = linreg(df, which="monthly")
-        return r**2
-    elif which == "yearly":
-        _, _, r, _, _ = linreg(df, which="yearly")
-        return r**2
-    else:
-        raise ValueError("which must be 'monthly' or 'yearly'")
-
-def linreg_ab(df: pd.DataFrame, which: str):
-    """Returns slope and intercept of the linear regression model."""
+    """Returns slope, intercept, r, p, std_err of the linear 
+    regression model using scipy.stats.linregress.
     
-    if which == "monthly":
-        x = mean(df, which="monthly")
-        t = np.arange(1, len(x)+1, 1)
-    elif which == "yearly":
-        x = mean(df, which="yearly")
-        t = hyd_years(df)
-    
-    n = len(df)    
+    Alternativ:
+    x, t, n = __preprocess(df, which)
     mean_x = np.mean(x)
     mean_t = np.mean(t)
     sum_xt = sum([x_i * t_i for x_i, t_i in zip(x, t)])
@@ -48,12 +34,15 @@ def linreg_ab(df: pd.DataFrame, which: str):
     slope = (sum_xt - n*mean_x*mean_t) / (sum_t2 - n*mean_t**2)
     intercept = mean_x - slope*mean_t
     Linreg = namedtuple("LineareRegression", ["slope", "intercept"])
-    l = Linreg(slope, intercept)
-    return l
+    return Linreg(slope, intercept)
+    """
 
-def trendtest(df: pd.DataFrame):
-    # TODO: #2 Implement t-test for trend
-    pass
+    x, t, _ = __preprocess(df, which)
+    return scipy.stats.linregress(t, x, alternative="two-sided")
+
+def test_statistic(df: pd.DataFrame, which: str):
+    """Returns test statistic for the trend test with yearly values."""
+    return linreg(df, which=which).slope / linreg(df, which=which).stderr
 
 def detrend_signal(df: pd.DataFrame) -> None:
     """Detrend the time series."""
