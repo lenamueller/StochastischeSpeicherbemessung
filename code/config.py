35e1ab433104282c3a053d6ee2ff0f9bba8eb795
Lena Mueller <lenamariemue@gmail.com>
2023-11-12 17:30:55 +0100

    Add remapper to config

--- code/config.py
+++ code/config.py
@@ -5,6 +5,18 @@ report_path = "reports/"
 
 fn_results = f"data/Daten_{pegelname}_results.csv"
 
+var_remapper = {
+        "Durchfluss_m3s": "Rohdaten",
+        "trendber": "Trendbereinigt",
+        "saisonfigur_mean": "Saisonale Komp. (Mittel)",
+        "saisonfigur_std": "Saisonale Komp. (Standardabweichung)",
+        "saisonber": "Saisonbereinigt",
+        "normier": "Normiert",
+        "autokorr_saisonfigur": "Autokorr. (Saisonfigur)",
+        "autokorr": "Autokorr. Komp.",
+        "zufall": "Zufallskomp."
+    }
+
 tu_darkblue = (0/255, 48/255, 93/255)
 tu_mediumblue = (0/255, 105/255, 180/255)
 tu_grey = (114/255, 119/255, 119/255)
