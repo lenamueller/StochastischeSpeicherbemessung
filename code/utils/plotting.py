🐛 Fix Thomas Fiering calculation. Plot generated time series.

code/utils/plotting.py
@@ -435,12 +435,41 @@ def plot_monthly_fitting(df: pd.DataFrame) -> None:
     axs[0, 0].legend()
     plt.savefig(f"{image_path}/{pegelname}_fit.png", dpi=300, bbox_inches="tight")
 
-    y_true = st.binned_stats(df, var="Durchfluss_m3s", bin="monthly", func=np.mean)
-    plt.plot(x, y_true, label="Rohdaten", color="k")
-    
-    for _ in range(n):
-        plt.plot(x, thomasfiering(df), color="grey", alpha=0.1)
+def plot_thomasfiering(df: pd.DataFrame, gen_data: np.array, n: int = 10) -> None:
+    """Plot the generated data from the Thomas Fiering model."""
 
+    x = np.arange(0, 12)
+    arr = np.reshape(df["Durchfluss_m3s"].to_numpy(), (-1, 12))
+    
+    plt.figure(figsize=(12, 5))
+    
+    # raw data
+    plt.plot(x, np.mean(arr, axis=0), color=tu_red, alpha=1, lw=1, 
+             label=f"Mittel der Rohdaten (40 Zeitreihen)")
+    # for row_i in range(len(arr)):
+    #     plt.plot(x, arr[row_i], color=tu_red, alpha=0.3, lw=0.5)
+    plt.boxplot(arr, positions=x-0.1, widths=0.1, patch_artist=True,
+                boxprops=dict(facecolor=tu_red, color=tu_red, alpha=0.2),
+                capprops=dict(color=tu_red, alpha=0.3),
+                whiskerprops=dict(color=tu_red, alpha=0.3),
+                flierprops=dict(color=tu_red, alpha=0.3, markeredgecolor=tu_red),
+                medianprops=dict(color=tu_red, alpha=0.3))
+    
+    # generated data
+    plt.plot(x, np.mean(gen_data, axis=0), color="k", alpha=1, lw=1, 
+             label=f"Mittel der generierten Zeitreihen ({len(gen_data)} Zeitreihen)")
+    for i in range(min(len(gen_data), n)):
+        plt.plot(x, gen_data[i], color="grey", alpha=0.1, lw=0.5)
+    plt.fill_between(x, np.min(gen_data, axis=0), np.max(gen_data, axis=0), 
+            color="grey", alpha=0.1, label="Spannweite der generierten Zeitreihen")
+    plt.boxplot(gen_data, positions=x+0.1, widths=0.1, patch_artist=True,
+                boxprops=dict(facecolor="grey", color="grey", alpha=0.2),
+                capprops=dict(color="grey", alpha=0.3),
+                whiskerprops=dict(color="grey", alpha=0.3),
+                flierprops=dict(color="grey", alpha=0.3, markeredgecolor="grey"),
+                medianprops=dict(color="grey", alpha=0.3))
+    
+    plt.grid(color="grey", alpha=0.3)
     x_labels = ["N", "D", "J", "F", "M", "A", "M", "J", "J", "A", "S", "O"]
     plt.xticks(x, x_labels)
     plt.legend()

code/utils/thomasfiering.py
@@ -1,14 +1,10 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import lognorm
+from scipy.stats import skew
 
-from config import T
 from utils.statistics import hyd_years
+from utils.data_structures import _monthly_vals
 
-
-def _monthly_vals(df: pd.DataFrame, i: int)-> np.ndarray:
-    df = df[df["Monat"].str.startswith(str(i).zfill(2))]
-    return df["Durchfluss_m3s"].to_numpy()
 
 def index_month_before(i: int) -> int:
     """Return the index of the month before."""
@@ -54,14 +50,7 @@ def parameter_rp(df: pd.DataFrame, i:int) -> float:
     else:
         raise ValueError("r_p is not in [-1,1] for month", i)
 
-def fit_lognorm(df: pd.DataFrame, i: int) -> tuple[float, float, float]:
-    x = _monthly_vals(df, i)
-    shape, loc, scale = lognorm.fit(x)
-    return shape, loc, scale
-
-def thomasfiering(
-        df: pd.DataFrame
-        ) -> float:
+def thomasfiering(df: pd.DataFrame) -> float:
     """
     Returns a time series with monthly discharge values 
     generated with the Thomas Fiering model.
@@ -80,18 +69,22 @@ def thomasfiering(
         rp = parameter_rp(df, i)
         xp_before = parameter_xp(df, index_month_before(i))
         sp_before = parameter_sp(df, index_month_before(i))
+        rp_before = parameter_rp(df, index_month_before(i))
+        
+        x_new = -1
+        while x_new <= 0.1:
+            ti = np.random.normal(loc=0, scale=1, size=1)[0] # random value from normal distr.
+            csi = skew(_monthly_vals(df, i), bias=False) # sample skewness
+            cti = (csi - rp_before**3*csi - 1) / ((1 - rp**2))**(3/2)
+            tg = 2/cti * (1 + (cti*ti)/6 - cti**2/36) - 2/cti # random value from gamma distr.
 
-        # TODO: random number from log-normal distribution        
-        # shape, loc, scale = fit_lognorm(df, i)
-        # ti = lognorm.rvs(s=shape, loc=loc, scale=scale, size=1, random_state=None)[0]
+            term1 = xp # mean term
+            term2 = (rp * sp/sp_before) * (value_month_before - xp_before) # middle term
+            term3 = tg*sp*np.sqrt(1-rp**2) # random term
+            x_new = term1 + term2 + term3
         
-        # TODO: get random value of normal distribution
-        ti = np.random.normal(0, 1, 1)[0]
-        
-        term1 = xp
-        term2 = (rp * sp/sp_before) * (value_month_before - xp_before)
-        term3 = ti*sp*np.sqrt(1-rp**2)
-        x_new = term1 + term2 + term3
+        if x_new <= 0:
+            raise ValueError("Negative value generated!")
         
         new_time_series.append(x_new)
 