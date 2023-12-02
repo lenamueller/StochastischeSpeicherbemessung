207fc71fa9617ea069f508982945af8608c1d226
Lena Mueller <lenamariemue@gmail.com>
2023-12-02 15:55:06 +0100

    NOSIG

--- code/main_generate_timeseries.py
+++ code/main_generate_timeseries.py
@@ -16,6 +16,30 @@ check_path(report_path)
 
 raw_data = read_data(f"data/others/Daten_{pegelname}.txt")
 
+
+# -----------------------------------------
+# Generate time series
+# -----------------------------------------
+
+gen_arr = np.array([thomasfiering(raw_data, n_years=N_YEARS) for _ in range(N_TIMESERIES)])
+gen_data = pd.DataFrame(
+    data=gen_arr.transpose(),
+    columns=[f"G{str(i).zfill(3)}" for i in range(1, N_TIMESERIES+1)]
+    )
+gen_data["Monat"] = raw_data["Monat"][:len(gen_arr[0])]
+print(gen_data)
+
+
+gen_data.to_csv(
+    f"data/{pegelname}_thomasfiering_timeseries.csv", index=True)
+gen_data.iloc[:].round(3).to_latex(
+    f"data/{pegelname}_thomasfiering_timeseries.tex", index=True)
+gen_data.iloc[:, :10].round(3).to_latex(
+    f"data/{pegelname}_thomasfiering_timeseries_first10.tex", index=True)
+
+plot_thomasfierung_eval(raw_data, gen_data)
+
+
 # -----------------------------------------
 # Fit model parameters (Thomas-Fiering model)
 # -----------------------------------------
@@ -30,33 +54,11 @@ tf_pars = pd.DataFrame(
     index=[MONTH[i] for i in MONTH_HYD_YEAR]
 )
 
-tf_pars = tf_pars.round(4)
-tf_pars.to_csv(f"data/{pegelname}_tomasfiering_parameters.csv", index=True)
-tf_pars.to_latex(f"data/{pegelname}_tomasfiering_parameters.tex", index=True)
+tf_pars.round(4).to_csv(f"data/{pegelname}_tomasfiering_parameters.csv", index=True)
+tf_pars.round(4).to_latex(f"data/{pegelname}_tomasfiering_parameters.tex", index=True)
 
 # -----------------------------------------
 # Fit Gamma and LogNV to raw data
 # -----------------------------------------
 
 plot_monthly_fitting(raw_data)
-
-# -----------------------------------------
-# Generate time series
-# -----------------------------------------
-
-gen_arr = np.array([thomasfiering(raw_data, n_years=N_YEARS) for _ in range(N_TIMESERIES)])
-
-gen_data = pd.DataFrame(
-    data=gen_arr.transpose(),
-    index=raw_data["Monat"],
-    columns=[f"G{str(i).zfill(3)}" for i in range(1, N_TIMESERIES+1)]
-    )
-
-gen_data.to_csv(
-    f"data/{pegelname}_thomasfiering_timeseries.csv", index=True)
-gen_data.iloc[:].round(3).to_latex(
-    f"data/{pegelname}_thomasfiering_timeseries.tex", index=True)
-gen_data.iloc[:, :10].round(3).to_latex(
-    f"data/{pegelname}_thomasfiering_timeseries_first10.tex", index=True)
-
-plot_thomasfierung_eval(raw_data, gen_data)
