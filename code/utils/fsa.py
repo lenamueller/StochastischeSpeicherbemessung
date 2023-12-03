🐛 Fix bug in capacity calculation Bug: Unit of inflow was m3s not hm3

code/utils/fsa.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
-from utils.statistics import binned_stats
-from config import ALPHA, ABGABEN, SEC_PER_MONTH, N_TIMESERIES
+from utils.plotting import plot_monthly_discharge, plot_fsa, \
+    plot_storage
+from config import pegelname, ALPHA, ABGABEN, SEC_PER_MONTH, \
+    N_TIMESERIES, MONTH_HYD_YEAR_TXT
 
 
 def monthly_discharge(arr: np.ndarray) -> dict:
@@ -35,7 +36,7 @@ def monthly_discharge(arr: np.ndarray) -> dict:
         10:     mean_discharge * ABGABEN[10]/100 * ALPHA
     }
 
-def calc_storage(
+def storage_sim(
         q_in: np.ndarray, 
         q_out: np.ndarray, 
         initial_storage: float = 0,
@@ -144,3 +145,94 @@ def calc_capacity(storage: np.ndarray) -> tuple[float, int]:
         return cap, cap_min_index, cap_min, cap_max_index, cap_max
     else: 
         return 0, 0, 0, 0, 0
+
+def fsa(raw_data: pd.DataFrame, gen_data: pd.DataFrame):
+    
+    print("\n--------------------------------------")
+    print("\nBerechnung monatlicher Soll-Abgaben\n")
+    
+    monthly_dis = {}
+    monthly_dis["original"] = monthly_discharge(raw_data["Durchfluss_m3s"].to_numpy())
+    for i in range(N_TIMESERIES):
+        monthly_dis[f"G{str(i+1).zfill(3)}"] = monthly_discharge(
+            gen_data[f"G{str(i+1).zfill(3)}_m3s"].to_numpy())
+
+    monthly_dis = pd.DataFrame.from_dict(monthly_dis)
+    monthly_dis.index = MONTH_HYD_YEAR_TXT
+    monthly_dis = monthly_dis.transpose()
+    monthly_dis.round(3).to_csv(f"data/{pegelname}_monthly_discharge.csv", index=True)
+    monthly_dis.round(3).to_latex(f"data/{pegelname}_monthly_discharge.tex", index=True)
+    print(f"-> data/{pegelname}_monthly_discharge.csv")
+    
+    plot_monthly_discharge(monthly_dis)
+    
+    print("\n--------------------------------------")
+    print("\nBerechnung Speicherkapazität\n")
+    
+    capacities = {}
+    
+    # -----------------------------------------
+    #   convert inflow from m³/s to hm³
+    # -----------------------------------------
+    
+    raw_data["Durchfluss_hm3"] = raw_data["Durchfluss_m3s"] * SEC_PER_MONTH/1000000
+    print(raw_data)
+    print(gen_data)
+    # gen_data = gen_data.transpose()
+    # print(gen_data)
+    
+    for i in range(N_TIMESERIES):
+        gen_data[f"G{str(i+1).zfill(3)}_hm3"] = \
+            gen_data[f"G{str(i+1).zfill(3)}_m3s"] * SEC_PER_MONTH/1000000
+    print(gen_data)
+    
+    # -----------------------------------------
+    #               original data
+    # -----------------------------------------
+    
+    q_in = raw_data["Durchfluss_hm3"].to_numpy()
+    q_out = np.tile(monthly_dis.loc["original", :].to_numpy(), 40)
+    
+    storage, _, _, _ = storage_sim(q_in, q_out, initial_storage=0, max_cap=np.inf)
+    
+    cap, cap_min_index, cap_min, _, cap_max = calc_capacity(storage)
+    capacities["original"] = cap
+    
+    # Plot FSA for original data
+    max_vals, max_indices = calc_maxima(storage=storage)
+    min_vals, min_indices = calc_minima(storage=storage, max_indices=max_indices)
+    plot_fsa(storage, max_vals=max_vals, max_indices=max_indices, 
+             min_vals=min_vals, min_indices=min_indices, cap=cap, 
+             cap_min_index=cap_min_index, cap_min=cap_min, cap_max=cap_max)
+
+    # Plot storage simulation for unlimited reservoir
+    storage, deficit, overflow, q_out_real = storage_sim(
+        q_in, q_out, initial_storage=0, max_cap=np.inf)
+    plot_storage(q_in, q_out, q_out_real, storage, deficit, overflow, 
+                fn_ending="unlimited")
+
+    # Plot storage simulation for limited reservoir
+    storage, deficit, overflow, q_out_real = storage_sim(
+        q_in, q_out, initial_storage=0, max_cap=cap)
+    plot_storage(q_in, q_out, q_out_real, storage, deficit, overflow, 
+                fn_ending=str(round(cap, 3)))
+    
+    # -----------------------------------------
+    #            generated data
+    # -----------------------------------------
+    
+    for i in range(N_TIMESERIES):
+        q_in = gen_data[f"G{str(i+1).zfill(3)}_hm3"]
+        q_out = np.tile(monthly_dis.loc[f"G{str(i+1).zfill(3)}", :].to_numpy(), 40)
+        storage, _, _, _ = storage_sim(q_in, q_out, initial_storage=0, max_cap=np.inf)
+        cap, _, _, _, _ = calc_capacity(storage)
+        capacities[f"G{str(i+1).zfill(3)}"] = cap
+
+    df_capacities = pd.DataFrame()
+    df_capacities["Zeitreihe"] = capacities.keys()
+    df_capacities["Kapazität"] = capacities.values()
+
+    df_capacities.to_csv(f"data/{pegelname}_capacities.csv", index=False)
+    df_capacities.to_latex(f"data/{pegelname}_capacities.tex", index=False)
+    
+    print(df_capacities)

code/utils/plotting.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import pymannkendall as mk
 import scipy
 import seaborn as sns
 from scipy.stats import lognorm, gamma
@@ -10,7 +9,6 @@ from config import image_path, pegelname, tu_mediumblue, tu_grey, tu_red, \
     var_remapper, N_TIMESERIES, MONTH_ABB
 import utils.statistics as st
 from utils.data_structures import _monthly_vals
-from utils.fsa import calc_maxima, calc_minima, calc_capacity
 
 
 def plot_raw(df: pd.DataFrame) -> None:
@@ -453,7 +451,7 @@ def plot_thomasfierung_eval(raw_data: pd.DataFrame, gen_data: pd.DataFrame):
     hists = []
     
     for i in range(N_TIMESERIES):
-        gen_i = gen_data[f"G{str(i+1).zfill(3)}"].to_numpy().reshape(-1, 12)
+        gen_i = gen_data[f"G{str(i+1).zfill(3)}_m3s"].to_numpy().reshape(-1, 12)
     
         m = np.mean(gen_i, axis=0)
         v = np.var(gen_i, axis=0)
@@ -560,11 +558,17 @@ def plot_storage(
     plt.tight_layout()
     plt.savefig(image_path+f"{pegelname}_storage_{fn_ending}.png", dpi=300, bbox_inches="tight")
 
-def plot_fsa(storage: np.ndarray):
-    
-    max_vals, max_indices = calc_maxima(storage)
-    min_vals, min_indices = calc_minima(storage, max_indices)
-    cap, cap_min_index, cap_min, cap_max_index, cap_max = calc_capacity(storage)
+def plot_fsa(
+        storage: np.ndarray,
+        max_vals: list[float],
+        max_indices: list[float],
+        min_vals: list[float],
+        min_indices: list[float],
+        cap: float,
+        cap_min_index: float, 
+        cap_min: float,
+        cap_max: float
+        ):
     
     plt.figure(figsize=(15,8))
     plt.title(f"Maximalkapazität des Speichers: {round(cap, 3)} hm³",
@@ -583,7 +587,6 @@ def plot_fsa(storage: np.ndarray):
              va="bottom", fontsize=10, color="k")
     plt.text(100, cap_max, f"{round(cap_max, 3)} hm³", ha="left", 
              va="bottom", fontsize=10, color="k")
-    
     
     plt.legend(loc="upper center", fontsize=10)
     plt.grid(color="grey", alpha=0.3)

code/utils/thomasfiering.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import numpy as np
 from scipy.stats import skew
+import time
 
+from config import pegelname, N_TIMESERIES, N_YEARS, MONTH_HYD_YEAR, MONTH
+from utils.plotting import plot_monthly_fitting, plot_thomasfierung_eval
 from utils.statistics import hyd_years
 from utils.data_structures import _monthly_vals
 
@@ -61,7 +64,7 @@ def parameter_rp(df: pd.DataFrame, i:int) -> float:
     else:
         raise ValueError("r_p is not in [-1,1] for month", i)
 
-def thomasfiering(df: pd.DataFrame, n_years: int = 1) -> float:
+def gen_timeseries(df: pd.DataFrame, n_years: int = 1) -> float:
     """
     Returns a time series with monthly discharge values starting
     in November ending in October, generated with the Thomas-Fiering 
@@ -116,3 +119,44 @@ def thomasfiering(df: pd.DataFrame, n_years: int = 1) -> float:
                 new_time_series.append(x_new)
 
     return new_time_series
+
+def thomasfiering(raw_data: pd.DataFrame):
+    print("\n--------------------------------------")
+    print("\nZeitreihengenerierung\n")
+    
+    start_time = time.time()
+    
+    # calculate parameters
+    tf_pars = pd.DataFrame(
+        data={
+            "Mittelwert": [parameter_xp(raw_data, i) for i in MONTH_HYD_YEAR],
+            "Standardabweichung": [parameter_sp(raw_data, i) for i in MONTH_HYD_YEAR],
+            "Korrelationskoeffizient": [parameter_rp(raw_data, i) for i in MONTH_HYD_YEAR]
+        },
+        index=[MONTH[i] for i in MONTH_HYD_YEAR]
+    )
+
+    tf_pars.round(4).to_csv(f"data/{pegelname}_tomasfiering_parameters.csv", index=True)
+    tf_pars.round(4).to_latex(f"data/{pegelname}_tomasfiering_parameters.tex", index=True)
+    print(f"-> Modellparameter: data/{pegelname}_tomasfiering_parameters.csv")
+    
+    # generate data
+    gen_data = pd.DataFrame()
+    gen_data["Monat"] = raw_data["Monat"]
+    for i in range(N_TIMESERIES):
+        gen_data[f"G{str(i+1).zfill(3)}_m3s"] = gen_timeseries(raw_data, n_years=N_YEARS)
+        
+    gen_data.to_csv(
+        f"data/{pegelname}_thomasfiering_timeseries.csv", index=False)
+    gen_data.iloc[:].round(3).to_latex(
+        f"data/{pegelname}_thomasfiering_timeseries.tex", index=False)
+    gen_data.iloc[:, :10].round(3).to_latex(
+        f"data/{pegelname}_thomasfiering_timeseries_first10.tex", index=False)
+
+    print("-> generierte Zeitreihen: data/{pegelname}_thomasfiering_timeseries.csv")
+    print("Dauer der Generierung:", round(time.time()-start_time, 1), "Sekunden")
+    
+    plot_thomasfierung_eval(raw_data=raw_data, gen_data=gen_data)
+    plot_monthly_fitting(raw_data)
+    
+    return gen_data