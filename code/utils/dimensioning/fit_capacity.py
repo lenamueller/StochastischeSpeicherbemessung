cd15d027f2cf9d137ed5410e702ce7f43cf2670e
Lena Müller <lenamariemue@gmail.com>
2023-12-05 12:17:44 +0100

    🗃️ Update float format

--- code/utils/dimensioning/fit_capacity.py
+++ code/utils/dimensioning/fit_capacity.py
@@ -14,43 +14,63 @@ def fit_capacity():
     print("\n--------------------------------------")
     print("\nKapazität für 90 % Zuverlässigkeit\n")
     
+    # -----------------------------------------
     # read capacities
+    # -----------------------------------------
+    
     cap = pd.read_csv(f"data/{pegelname}_capacities_100.csv") # always use sample size 100
     cap = cap[cap["Zeitreihe"] != "original"]
     cap_sort = sorted(cap["Kapazität"])
     
+    # -----------------------------------------
+    # emp. and theo. Pu
+    # -----------------------------------------
+    
+    rangzahlen = np.arange(1, len(cap)+1)
     pu_emp = pu_weibull(n=len(cap))
+
     shape, loc, scale = lognorm.fit(cap_sort)
     print(f"LogNV Parameter: Shape = {shape}, Loc = {loc}, Scale = {scale}")
-    print(f"Stats: {lognorm.stats(shape, loc, scale)}")
-    
+    print(f"Mean: {lognorm.mean(shape, loc, scale)}")
+    print(f"Std: {lognorm.std(shape, loc, scale)}")
     pu_theo = lognorm.cdf(x=cap_sort, s=shape, loc=loc, scale=scale)
 
-    # quantiles from data
+    pd.DataFrame(data={"Rangzahl [-]": rangzahlen, "empirische Pu [-]": pu_emp, "theoretische Pu  [-]": pu_theo}).round(3).to_csv(
+        f"data/{pegelname}_pu.csv", index=False)
+    pd.DataFrame(data={"Rangzahl [-]": rangzahlen, "empirische Pu [-]": pu_emp, "theoretische Pu [-]": pu_theo}).round(3).to_latex(
+        f"data/{pegelname}_pu.tex", index=False, float_format="%.3f")
+    
+    # -----------------------------------------
+    # emp. and theo. quantiles
+    # -----------------------------------------
+    
     q_emp = cap_sort
     q_theo = lognorm.ppf(pu_emp, shape, loc=loc, scale=scale)
     
-    pd.DataFrame(data={"empQuantile": q_emp, "theoQuantile": q_theo}).round(3).to_csv(
-        f"data/{pegelname}_quantile.csv", index=False)
-    pd.DataFrame(data={"empQuantile": q_emp, "theoQuantile": q_theo}).round(3).to_latex(
-        f"data/{pegelname}_quantile.tex", index=False)
+    pd.DataFrame(data={"empirische Quantile [hm³]": q_emp, "theoretische Quantile [hm³]": q_theo}).round(3).to_csv(
+        f"data/{pegelname}_quantile.csv", index=True)
+    pd.DataFrame(data={"empirsiche Quantile [hm³]": q_emp, "theoretische Quantile [hm³]": q_theo}).round(3).to_latex(
+        f"data/{pegelname}_quantile.tex", index=True, float_format="%.3f")
     
-    # fixed quantiles
+    # -----------------------------------------
+    # theo. quantiles for fixed Pu
+    # -----------------------------------------
+    
     q = np.arange(0,1,0.05)
     q_theo_fixed = lognorm.ppf(q, shape, loc=loc, scale=scale)
 
-    pd.DataFrame(data={"Quantile":q, "theoQuantile":q_theo_fixed}).round(3).to_csv(
+    pd.DataFrame(data={"Pu":q, "theoretische Quantile [hm³]":q_theo_fixed}).round(3).to_csv(
         f"data/{pegelname}_quantile_fixed.csv", index=False)
-    pd.DataFrame(data={"Quantile":q, "theoQuantile":q_theo_fixed}).round(3).to_latex(
-        f"data/{pegelname}_quantile_fixed.tex", index=False)
+    pd.DataFrame(data={"Pu":q, "theoretische Quantile [hm³]":q_theo_fixed}).round(3).to_latex(
+        f"data/{pegelname}_quantile_fixed.tex", index=False, float_format="%.3f")
     
     # 90 % quantile
     cap_90 = lognorm.ppf(0.9, shape, loc=loc, scale=scale)
     print(f"90 % Quantile: {cap_90}")
     
-    plot_capacity(capacities_sort=cap_sort, pu_emp=pu_emp, pu_theo=pu_theo, 
-                  cap_90=cap_90)
-    qq_plot(emp=q_emp, theo=q_theo)
+    # -----------------------------------------
+    # test fitting
+    # -----------------------------------------
     
     # KS test
     print(kstest(pu_emp, pu_theo))
@@ -58,8 +78,9 @@ def fit_capacity():
     # rqq test
     print(f"r_qq = {np.corrcoef(q_emp, q_theo)[0][1]}")
     
-    
-    
-    
-    
+    # -----------------------------------------
+    # plot
+    # -----------------------------------------
 
+    plot_capacity(capacities_sort=cap_sort, pu_emp=pu_emp, pu_theo=pu_theo, cap_90=cap_90)
+    qq_plot(emp=q_emp, theo=q_theo)
